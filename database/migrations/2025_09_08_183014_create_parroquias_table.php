2affbc1eed4f859abfe17d423e8d2576c5a85c60
Vcsaransiga <vcsaransiga@utn.edu.ec>
2025-09-08 18:30:43 -0500

    tabla parroquias

---
+++ database/migrations/2025_09_08_183014_create_parroquias_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parroquias', function (Blueprint $t) {
+            $t->id();
+            $t->foreignId('canton_id')->constrained('cantones')->cascadeOnUpdate()->restrictOnDelete();
+            $t->string('nombre', 255);
+            $t->timestampsTz();
+            $t->unique(['canton_id', 'nombre']); // no duplicar por cantón
+        });
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('parroquias');
+    }
+};
